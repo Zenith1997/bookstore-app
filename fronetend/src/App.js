25e365a82b7d3888f627457c11d1fa104f01b100
Supul Kalhara <supulkalhara7@gmail.com>
2022-05-09 12:40:11 +0530

    Header

--- fronetend/src/App.js
+++ fronetend/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Header/>
     </div>
   );
 }
---
+++ fronetend/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {AppBar, Tabs, Toolbar, Typography} from "@mui/material";
+import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
+
+const Header = () => {
+
+    return (
+    
+        <div>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <Typography >
+                        <CollectionsBookmarkIcon />
+                    </Typography>
+                    <Tabs>
+                         
+                    </Tabs>
+                </Toolbar>
+               
+            </AppBar>
+        </div>
+    );
+}
+
+export default Header;

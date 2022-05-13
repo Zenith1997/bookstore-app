f7c62475a31a4fdf5945991ccd2660e18add3f96
Supul Kalhara <supulkalhara7@gmail.com>
2022-05-13 13:01:59 +0530

    home edited

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import "./Home.css"
+import React from 'react';
+
 
 const Home = () => {
   return (
     <div>
-      Home
-      <p>hi</p>
-      jhedjje
+      <div className='content'>
+        <div className="heading">
+        <h1>Book store</h1>
+        </div>
+    
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi numquam nemo, maiores cumque expedita amet? Aut nihil ex nesciunt inventore perferendis totam maxime quod. Officiis consequatur non repellendus ratione hic.
+      </p>
+      </div>
+     
+          <button>sfgg</button>
     </div>
   )
 }
